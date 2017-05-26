31413c1bfcce40cbf42b4f4c7e13c9b761def2aa
Raúl Ferràs <raul@viscaweb.com>
2017-05-26 11:04:56 +0200

    Redefine object returned.

--- src/Model/TableRenderedModel.php
+++ src/Model/TableRenderedModel.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Visca\WebTableFan\Model;
 
-use Visca\WebTableFan\Entity\Node\Node;
+use Visca\WebTableFan\Entity\Node\TableNode;
 
 /**
  * Class TableRenderedModel.
@@ -14,33 +14,27 @@ class TableRenderedModel
     /** @var string */
     private $html;
 
-    /** @var Node|null */
+    /** @var TableNode|null */
     private $tableNode;
 
     /**
      * TableRenderedModel constructor.
      *
      * @param string    $html
-     * @param Node|null $tableNode
+     * @param TableNode|null $tableNode
      */
-    public function __construct(string $html, Node $tableNode = null)
+    public function __construct(string $html, TableNode $tableNode = null)
     {
         $this->tableNode = $tableNode;
         $this->html = $html;
     }
 
-    /**
-     * @return string
-     */
     public function getHtml(): string
     {
         return $this->html;
     }
 
-    /**
-     * @return Node
-     */
-    public function getTableNode(): Node
+    public function getTableNode(): TableNode
     {
         return $this->tableNode;
     }
